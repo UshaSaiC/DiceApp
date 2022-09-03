67bdcad58c8ee8440e026e383f41680e8d2efd56
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-03 17:27:50 +0530

    Added required assets along with conditions that turns both dice values to 4 when clikced on roll button

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlet is used to refer to an UI element
+    //Below variables are automatically created by establishing a connection i.e clicking on control button, selecting desired UI element from Main.storyboard and draging the reference outlets from the element and dropping it in this file
+    @IBOutlet weak var rightSideDiceImageView: UIImageView!
+    @IBOutlet weak var leftSideDiceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //changing the properties of UI elements during run time
+        // for below values used #imageLiteral( the an enter button to insert images in code file
+        //object.property = value
+        rightSideDiceImageView.image = #imageLiteral(resourceName: "DiceSix")
+        leftSideDiceImageView.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
+    //IBAction is triggered when a particular action happens on UI element
+    @IBAction func rollButtonClicked(_ sender: Any) {
+        print("Button is tapped)")
+        rightSideDiceImageView.image = #imageLiteral(resourceName: "DiceFour")
+        leftSideDiceImageView.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 

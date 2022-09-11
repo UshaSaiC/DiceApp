90cec71c391528d3ea4b48dfe00e480a986e00a9
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-11 20:06:24 +0530

    Added assets file and updated UI to show dice images and a roll button

--- DiceAppUI/ContentView.swift
+++ DiceAppUI/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .aspectRatio(contentMode: .fill)
+            VStack {
+                Image("diceeLogo")
+                //Spacer is used to create space between components
+                Spacer()
+                HStack {
+                    DiceView(imageName: "dice1")
+                    DiceView(imageName: "dice2")
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                }){
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let imageName: String
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .frame(width: 150, height: 150)
+            .padding()
     }
 }

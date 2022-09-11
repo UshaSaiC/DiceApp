75cc7ff7802e4d883d781c08fefb70b36de1d0d4
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-11 19:40:00 +0530

    Initial Commit

---
+++ DiceAppUI/DiceAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppUIApp.swift
+//  DiceAppUI
+//
+//  Created by Usha Sai Chintha on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
